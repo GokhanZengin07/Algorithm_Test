69bc6dd3ad720c22824420f486e897e10cce8b2d
Ibrahim Gokhan Zengin <igokhanzengin@gmail.com>
2022-02-07 23:57:58 +0300

    Add project files.

---
+++ Challenge1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Test
+{
+
+    public class Challenge1
+    {
+
+        public String Reverse2(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            { return input; }
+            char[] arr = input.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+        public string ReverseEachWord(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            string [] arr=input.Split(' ');
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result.Append(Reverse2(arr[i]));
+                if (i!=arr.Length-1)
+                {
+                    result.Append(' ');
+                } 
+            }
+
+            return result.ToString();
+
+        }
+
+    }
+
+  
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+
+namespace Algorithm_Test
+{
+    
+    
+    
+    public class Program
+
+    {   
+
+        static String Reverse2( string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            { return input; }
+            char[]arr=input.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+        static String Reverse(string input)
+        {
+            if(string.IsNullOrEmpty(input))
+            { return input; }
+            StringBuilder reversed = new StringBuilder(input.Length);
+            for (int i = input.Length-1; i >=0;i--)
+            {
+                reversed.Append(input[i]);
+            }
+            return reversed.ToString();
+
+        }
+        static void ParseContents(string s)
+        {
+            Console.WriteLine("Option 1");
+            foreach (var ch in s)
+            {
+                Console.WriteLine(ch);
+            }
+            Console.WriteLine("Option 2");
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                Console.WriteLine(c);
+            }
+        }
+        static Boolean IsAtEvenIndex(string s,char item)
+        {
+            if (string.IsNullOrEmpty(s))
+
+            {
+                return false;
+            }
+            for (int i = 0; i < s.Length/2+1; i=i+2)
+            {
+                if(s[i]==item)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        static string NormalizeString(String input)
+        {
+            return input.ToLower().Trim().Replace(",", "");
+
+                
+        }
+        static void Main(string[] args)
+
+           
+        {
+            Console.WriteLine(reve);
+            Console.WriteLine(Reverse2("asd"));
+            Console.WriteLine(Reverse("asd"));
+            Console.WriteLine(IsAtEvenIndex("HelLo",'l'));
+            Console.WriteLine(IsAtEvenIndex(null, 'l'));
+            ParseContents("asdwe");
+            Console.WriteLine(maxNumber(5, 6, 8));
+            Console.ReadLine();
+            Console.WriteLine(maxNumber2(5, 61 ,8));
+            Console.ReadLine();
+            Console.WriteLine(isUpper("HeLoo"));
+            Console.ReadLine();
+            Console.WriteLine(isUpper("heloo"));
+            Console.ReadLine();
+            Console.WriteLine(NormalizeString("heloo   , asd"));
+            Console.ReadLine();
+            Console.WriteLine("asdasd".Contains("as"));
+
+        }
+        static Boolean isPasswordComplex(string s)
+        {   
+            return s.Any(char.IsUpper) && s.Any(char.IsLower) && s.Any(char.IsDigit);
+        }
+        static Boolean isUpper(string s)
+        {
+            return s.All(char.IsUpper);
+        }
+        static Boolean isLower(string s)
+        {
+            return s.All(char.IsLower);
+        }
+        static int maxNumber(int a, int b, int c)
+        {
+            if (a > b)
+            {
+                if (a > c || a == c)
+                {
+                    return a;
+                }
+            }
+            if (b > c)
+            {
+                if (b > a || b == a)
+                {
+                    return b;
+                }
+            }
+
+            return c;
+        }
+        static int maxNumber2(int a, int b, int c)
+        {
+            int max = a;
+            if(b>max)
+            { max = b;
+            }
+            if (c > max)
+            {
+                max = c;
+            }
+            return max;
+        }
+    }
+}
+
+
+
